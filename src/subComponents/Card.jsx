5370837f2cf362d5523386497a4086f4efa0938a
lehung2022 <hung.le.programmer@gmail.com>
2024-11-18 02:34:42 +0700

    added rel=noreferrer

--- src/subComponents/Card.jsx
+++ src/subComponents/Card.jsx
@@ -103,10 +103,10 @@ const Card = (props) => {
         })}
       </Tags>
       <Footer>
-        <Link href={demo} target="_blank">
+        <Link href={demo} target="_blank" rel="noreferrer">
           Visit
         </Link>
-        <Git href={github} target="_blank">
+        <Git href={github} target="_blank" rel="noreferrer">
           <Github width={30} height={30} />
         </Git>
       </Footer>
